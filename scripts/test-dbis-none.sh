1aa9ffddeed2067668045d9c6e24cefc1085b0b6
nmiranda <nicolas.miranda@hu-berlin.de>
2020-12-21 16:07:46 +0100

    DBIS none script

--- scripts/test-dbis-none.sh
+++ scripts/test-dbis-none.sh
@@ -1,3 +1,3 @@
-make-dataset /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test
+make-dataset /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test /home/miranda/ztf-rapid/data/processed/test_none/test_none.npz
 
-train-model /home/miranda/ztf-rapid/data/processed/test_under/test_under_01.npz /home/miranda/ztf-rapid/models/test_under/test_under_01.hdf5 /home/miranda/ztf-rapid/reports/figures/test
+train-model /home/miranda/ztf-rapid/data/processed/test_none/test_none.npz /home/miranda/ztf-rapid/models/test_none/test_none.hdf5 /home/miranda/ztf-rapid/reports/figures/test_none
