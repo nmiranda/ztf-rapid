626a6d7a2171bb6fc53485ca6b4b18edeb7ccd97
nmiranda <nicolas.miranda@hu-berlin.de>
2021-01-03 05:08:18 +0100

    Noisification script dbis

---
+++ scripts/test-noisify-dbis.sh
@@ -0,0 +1,5 @@
+make-dataset /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test_noisify /home/miranda/ztf-rapid/data/interim/test_noisify/test_noisify.npz --nocv
+
+noisify /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test_noisify/test_noisify.npz /home/miranda/ztf-rapid/data/processed/test_noisify/test_noisify.npz --epc 100
+
+train-model /home/miranda/ztf-rapid/data/processed/test_noisify/test_noisify.npz /home/miranda/ztf-rapid/models/test_noisify/test_noisify.hdf5 /home/miranda/ztf-rapid/reports/figures/test_noisify
