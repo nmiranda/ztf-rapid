322673991bb0c5ed0e9f69bf7446a24606321ea1
nmiranda <nicolas.miranda@hu-berlin.de>
2021-03-17 14:28:52 +0100

    Noisified classification with augmented test set

--- scripts/test-noisify-augtest-dbis.sh
+++ scripts/test-noisify-augtest-dbis.sh
@@ -1,4 +1,4 @@
-#make-dataset /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test_noisify /home/miranda/ztf-rapid/data/interim/test_noisify/test_noisify.npz --nocv
+make-dataset /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test_noisify /home/miranda/ztf-rapid/data/interim/test_noisify/test_noisify.npz --nocv
 
 noisify /home/miranda/ztf-rapid/data/raw/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv.pkl /home/miranda/ztf-rapid/data/interim/test_noisify/test_noisify.npz /home/miranda/ztf-rapid/data/processed/test_noisify_augtest/test_noisify_augtest.npz --epc 2000 --augtest
 
