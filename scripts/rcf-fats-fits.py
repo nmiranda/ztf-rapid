e440c7794719bec19ac4db44351bc68650c348d2
nmiranda <nicolas.miranda@hu-berlin.de>
2021-05-12 12:49:27 +0200

    rcf fats fits dbis

--- scripts/rcf-fats-fits.py
+++ scripts/rcf-fats-fits.py
@@ -4,6 +4,8 @@ from FATS.Feature import FeatureSpace
 from astropy.table import Table
 
 bands = ('p48g', 'p48r', 'p48i')
+# exclude_list = ['interp1d', 'FluxPercentileRatioMid20', 'FluxPercentileRatioMid35', 'FluxPercentileRatioMid50', 'FluxPercentileRatioMid65', 'FluxPercentileRatioMid80', 'PercentDifferenceFluxPercentile']
+exclude_list = ['interp1d']
 
 lc_data = pd.read_pickle('/home/miranda/ztf-rapid/data/interim/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv_nozeroes.pkl')
 
@@ -15,12 +17,12 @@ for band in bands:
 
     for ztfid, lc in lc_data.items():
         lc = lc[lc['band'] == band]
-        if len(lc) <= 2:
+        if len(lc) < 2:
             continue
         this_fluxes = lc['flux']
         this_times = lc['mjd']
         this_errors = lc['fluxerr']
-        feature_space = FeatureSpace(Data=['magnitude', 'time', 'error'], featureList=None, excludeList=['interp1d', 'FluxPercentileRatioMid20', 'FluxPercentileRatioMid35', 'FluxPercentileRatioMid50', 'FluxPercentileRatioMid65', 'FluxPercentileRatioMid80', 'PercentDifferenceFluxPercentile'])
+        feature_space = FeatureSpace(Data=['magnitude', 'time', 'error'], featureList=None, excludeList=exclude_list)
         feature_space = feature_space.calculateFeature(np.array([this_fluxes, this_times, this_errors]))
         this_result = feature_space.result(method='dict')
         results_list.append(this_result)
