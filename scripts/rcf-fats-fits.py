44c5397a69a135038fce1b3f3048746d395eb140
nmiranda <nicolas.miranda@hu-berlin.de>
2021-05-12 11:49:13 +0200

    rcf fats fits test

---
+++ scripts/rcf-fats-fits.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from FATS.Feature import FeatureSpace
+from astropy.table import Table
+
+bands = ('p48g', 'p48r', 'p48i')
+
+lc_data = pd.read_pickle('/home/miranda/ztf-rapid/data/interim/rcf_marshallc_sncosmo_200114_2018classupdate_addedcv_nozeroes.pkl')
+
+for band in bands:
+    ztfid_list = list()
+    target_list = list()
+    results_list = list()
+    for ztfid, lc in lc_data.items():
+        lc = lc[lc['band'] == band]
+        if len(lc) < 2:
+            continue
+        this_fluxes = lc['flux']
+        this_times = lc['mjd']
+        this_errors = lc['fluxerror']
+        feature_space = FeatureSpace(featureList=None)
+        feature_space = feature_space.calculateFeature(np.array([this_fluxes, this_times, this_errors]))
+        this_result = feature_space.result()
+        # results_list.append(feature_space.result())
+        print(Table(this_result))
+        exit(0)
+
+    
